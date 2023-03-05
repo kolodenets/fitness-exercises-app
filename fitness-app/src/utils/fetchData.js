f7f13ea5495ae65c6184a1a9fbb529df7936fc66
Mikhail Kolodenets <mishakolode@gmail.com>
2023-03-05 19:01:24 +0300

    feat: add Detail, ExerciseVideos, ExerciseDetail

--- fitness-app/src/utils/fetchData.js
+++ fitness-app/src/utils/fetchData.js
@@ -6,7 +6,13 @@ export const exerciseOptions = {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
   }
 };
-
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
+};
 export const fetchData = async (url, exerciseOptions) => {
   const response = await fetch(url, exerciseOptions);
   const data = await response.json();
